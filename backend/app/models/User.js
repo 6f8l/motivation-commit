ab43e5dc8ac727da6477abe4893592ca893f581d
6f8l <atsuki02jojo@gmail.com>
2019-08-18 15:55:57 +0900

    Add bcyrpt algorithm

--- backend/app/models/User.js
+++ backend/app/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
     id: {
@@ -21,4 +24,21 @@ const UserSchema = new mongoose.Schema({
         required: true,
         unique: false
     }
+});
+
+UserSchema.pre('save', (next) => {
+    // Check if document is new or a new password has been set
+    if (this.isNew | this.isModified('password')) {
+        // Saving reference to this because of changing scopes
+        const document = this;
+        bcrypt.hash(document.password, saltRounds, (err, hashedPassword) => {
+            if (err) { next(err); }
+            document.password = hashedPassword;
+            next();
+        });
+    } else {
+        next();
+    }
 })
+
+module.exports = mongoose.model('User', UserSchema);
